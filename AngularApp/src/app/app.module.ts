309a4c99ec8fedf8742cb07cf3c2d4dde08b7ff9
thenewcreation <galatafir@yahoo.com>
2021-10-03 17:18:53 -0500

    add service

--- AngularApp/src/app/app.module.ts
+++ AngularApp/src/app/app.module.ts
@@ -3,14 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HttpClientModule } from '@angular/common/http';
+import { AnatomicalStructuresComponent } from './components/anatomical-structures/anatomical-structures.component'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AnatomicalStructuresComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- AngularApp/src/environments/environment.ts
+++ AngularApp/src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  baseUrl: 'https://asctb-api.herokuapp.com/v2/18lJe-9fq5fHWr-9HuFTzhWnmfygeuXs2bbsXO8vh1FU/0'
 };
 
 /*
