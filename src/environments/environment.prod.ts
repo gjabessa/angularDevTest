13463fc217cb22bd0e35dcc936bc6a345f7bba1a
thenewcreation <galatafir@yahoo.com>
2021-10-03 21:00:37 -0500

    production setup

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  baseUrl: 'https://asctb-api.herokuapp.com/v2/18lJe-9fq5fHWr-9HuFTzhWnmfygeuXs2bbsXO8vh1FU/0',
+  secondBaseUrl: 'https://www.ebi.ac.uk/ols/api/ontologies/uberon/terms?iri=http://purl.obolibrary.org/obo/'
 };
